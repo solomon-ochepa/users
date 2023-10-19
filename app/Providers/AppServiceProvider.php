240b1b71e33347fe0513ee7d6cf9c948c4d8b52d
Solomon O. Ochepa <solomonochepa@gmail.com>
2023-10-19 15:24:50 +0100

    General update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,6 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        Passport::ignoreMigrations();
+        Passport::ignoreRoutes();
+
         //
     }
 
@@ -19,6 +23,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        /*
+         * Once enabled, all of your client secrets will only be displayable to the user immediately after they are created.
+         * Since the plain-text client secret value is never stored in the database, it is not possible to recover the secret's value if it is lost.
+         */
+        // Passport::hashClientSecrets();
+
+        Passport::tokensExpireIn(now()->addDays(30));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(3));
+
         //
     }
 }
